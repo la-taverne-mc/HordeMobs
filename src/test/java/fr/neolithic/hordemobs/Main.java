fe0c2b349a6c1cde4e7fe0104b60c9028c9a55a6
Po-C4 <leflem.mateo2002@gmail.com>
2019-10-28 02:52:05 +0100

    Création du projet maven + ajout de l'executeur de commandes

---
+++ src/test/java/fr/neolithic/hordemobs/Main.java
@@ -0,0 +1,10 @@
+package fr.neolithic.hordemobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
